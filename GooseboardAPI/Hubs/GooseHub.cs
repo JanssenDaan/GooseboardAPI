5c4c9b6deb2ac8dbd0b6c958e629f5739d4ec158
JanssenDaan <78667894+JanssenDaan@users.noreply.github.com>
2022-05-20 22:58:17 +0200

    Updated

--- GooseboardAPI/Hubs/GooseHub.cs
+++ GooseboardAPI/Hubs/GooseHub.cs
@@ -9,13 +9,13 @@ namespace SignalRChat.Hubs
     {
         public static List<Users> Users = new List<Users>();
         //public bool connected { get; set; }
-        //public new MySqlConnectionStringBuilder builder = new MySqlConnectionStringBuilder
-        //{
-        //    Server = "gooseboarddb.ngrok.io",
-        //    UserID = "daanjeu_king",
-        //    Password = "Poepchinees1",
-        //    Database = "daanjeu_dbdaan",
-        //};
+        public new MySqlConnectionStringBuilder builder = new MySqlConnectionStringBuilder
+        {
+            Server = "mysql.lamdev.be",
+            UserID = "daanj",
+            Password = "pFFx5menVQPJGY9mrNnXZ4v5PfJRFhSf",
+            Database = "daanj_gooseboard",
+        };
 
 
         public async Task SendMessage(string user, string message)
@@ -37,7 +37,11 @@ namespace SignalRChat.Hubs
             {
                 obj.Other = Newtonsoft.Json.JsonConvert.SerializeObject(Users);
             }
-
+            if (obj.Code == "GOOSECODE_LOGIN")
+            {
+                obj.Other = GetUser(obj.Other);
+                obj.Reciever = obj.Sender;  
+            }
             //Seralizes back
             message = Newtonsoft.Json.JsonConvert.SerializeObject(obj);
             await Clients.All.SendAsync("ReceiveMessage", user, message);
@@ -49,5 +53,38 @@ namespace SignalRChat.Hubs
         //    MySqlConnection connection = new MySqlConnection(builder.ConnectionString);
 
         //}
+        public string GetUser(string other)
+        {
+            string pswrd = "";
+            var user = Newtonsoft.Json.JsonConvert.DeserializeObject<User>(other);
+            MySqlConnection connection = new MySqlConnection(builder.ConnectionString);
+
+            using (connection)
+            {
+                MySqlCommand comm = connection.CreateCommand();
+                comm.CommandText = $"SELECT password FROM users WHERE username = '" + user.Password + "';";
+                //    comm.Parameters.Add("?emaill", MySqlDbType.Text).Value = entryLoginEmail.Text;
+                using (MySqlCommand command = new MySqlCommand(comm.CommandText, connection))
+                {
+
+                    object psw = command.ExecuteScalar();
+                    if (psw != null)
+                    {
+                        pswrd = psw.ToString();
+                    }
+                }
+
+            }
+            if (pswrd == user.Password)
+            {
+                return "CORRECT";
+            }
+            else
+            {
+                return "INCORRECT";
+            }
+            return "";
+        }
     }
+   
 }
