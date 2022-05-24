41b551dd2f3a548609e92703ef0666acc6fe2f2c
JanssenDaan <78667894+JanssenDaan@users.noreply.github.com>
2022-05-24 14:24:52 +0200

    Updated

--- GooseboardAPI/Hubs/GooseHub.cs
+++ GooseboardAPI/Hubs/GooseHub.cs
@@ -9,18 +9,8 @@ namespace SignalRChat.Hubs
     {
         public static List<Users> Users = new List<Users>();
         //public bool connected { get; set; }
-        public new MySqlConnectionStringBuilder builder = new MySqlConnectionStringBuilder
-        {
-            Server = "mysql.lamdev.be",
-            UserID = "daanj",
-            Password = "pFFx5menVQPJGY9mrNnXZ4v5PfJRFhSf",
-            Database = "daanj_gooseboard",
-        };
-
-
         public async Task SendMessage(string user, string message)
         {
-
             //Deserialize
             var obj = Newtonsoft.Json.JsonConvert.DeserializeObject<Message>(message);
 
@@ -39,51 +29,11 @@ namespace SignalRChat.Hubs
             }
             if (obj.Code == "GOOSECODE_LOGIN")
             {
-                obj.Other = GetUser(obj.Other);
-                obj.Reciever = obj.Sender;  
+                obj.Reciever = obj.Sender;
             }
             //Seralizes back
             message = Newtonsoft.Json.JsonConvert.SerializeObject(obj);
             await Clients.All.SendAsync("ReceiveMessage", user, message);
-        }
-
-
-        //public void GetDatabase(string query)
-        //{
-        //    MySqlConnection connection = new MySqlConnection(builder.ConnectionString);
-
-        //}
-        public string GetUser(string other)
-        {
-            string pswrd = "";
-            var user = Newtonsoft.Json.JsonConvert.DeserializeObject<User>(other);
-            MySqlConnection connection = new MySqlConnection(builder.ConnectionString);
-
-            using (connection)
-            {
-                MySqlCommand comm = connection.CreateCommand();
-                comm.CommandText = $"SELECT password FROM users WHERE username = '" + user.Password + "';";
-                //    comm.Parameters.Add("?emaill", MySqlDbType.Text).Value = entryLoginEmail.Text;
-                using (MySqlCommand command = new MySqlCommand(comm.CommandText, connection))
-                {
-
-                    object psw = command.ExecuteScalar();
-                    if (psw != null)
-                    {
-                        pswrd = psw.ToString();
-                    }
-                }
-
-            }
-            if (pswrd == user.Password)
-            {
-                return "CORRECT";
-            }
-            else
-            {
-                return "INCORRECT";
-            }
-            return "";
         }
     }
    
